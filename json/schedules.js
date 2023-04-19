c81c6f293dd67c786a8fcce34bb06a11cfef371b
Splittikin <45697042+Splittikin@users.noreply.github.com>
2023-04-19 10:38:40 -0400

    Redo mistakenly undone changes

---
+++ json/schedules.js
@@ -0,0 +1,607 @@
+var JSON_sched = {
+	"000": {
+		"metadata": {
+			"name": "Unscheduled",
+			"color": "rgb(0, 0, 0)",
+			"needsCheck": false
+		},
+		"periods": [
+			{
+				"name": "No School",
+				"startTimeDigits": "00:00",
+				"endTimeDigits": "00:00",
+				"startTime": 0,
+				"endTime": 0,
+				"intraschedule": {},
+				"intraindex": -1
+			}
+		]
+	},
+	"1GLL28670": {
+		"metadata": {
+			"name": "Silver Day",
+			"color": "rgb(204, 204, 204)",
+			"needsCheck": true
+		},
+		"periods": [
+			{
+				"name": "Passing Period",
+				"startTimeDigits": "08:00",
+				"endTimeDigits": "08:30",
+				"startTime": 28800,
+				"endTime": 30600,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Silver Period 5",
+				"startTimeDigits": "08:30",
+				"endTimeDigits": "09:53",
+				"startTime": 30600,
+				"endTime": 35580,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Passing Period",
+				"startTimeDigits": "09:53",
+				"endTimeDigits": "10:01",
+				"startTime": 35580,
+				"endTime": 36060,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Silver Period 6",
+				"startTimeDigits": "10:01",
+				"endTimeDigits": "11:24",
+				"startTime": 36060,
+				"endTime": 41040,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Silver Period 7 & Lunch",
+				"startTimeDigits": "11:24",
+				"endTimeDigits": "13:24",
+				"startTime": 41040,
+				"endTime": 48240,
+				"intraschedule": {
+					"A": [
+						{
+							"name": "A Lunch",
+							"startTimeDigits": "11:24",
+							"endTimeDigits": "11:54",
+							"startTime": 41040,
+							"endTime": 42840
+						},
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "11:54",
+							"endTimeDigits": "11:59",
+							"startTime": 42840,
+							"endTime": 43140
+						},
+						{
+							"name": "Silver Period 7",
+							"startTimeDigits": "11:59",
+							"endTimeDigits": "13:24",
+							"startTime": 43140,
+							"endTime": 48240
+						}
+					],
+					"B": [
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "11:24",
+							"endTimeDigits": "11:32",
+							"startTime": 41040,
+							"endTime": 41520
+						},
+						{
+							"name": "Silver Period 7",
+							"startTimeDigits": "11:32",
+							"endTimeDigits": "11:54",
+							"startTime": 41520,
+							"endTime": 42840
+						},
+						{
+							"name": "B Lunch",
+							"startTimeDigits": "11:54",
+							"endTimeDigits": "12:24",
+							"startTime": 42840,
+							"endTime": 44640
+						},
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "12:24",
+							"endTimeDigits": "12:29",
+							"startTime": 44640,
+							"endTime": 44940
+						},
+						{
+							"name": "Silver Period 7",
+							"startTimeDigits": "12:29",
+							"endTimeDigits": "13:24",
+							"startTime": 44940,
+							"endTime": 48240
+						}
+					],
+					"C": [
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "11:24",
+							"endTimeDigits": "11:32",
+							"startTime": 41040,
+							"endTime": 41520
+						},
+						{
+							"name": "Silver Period 7",
+							"startTimeDigits": "11:32",
+							"endTimeDigits": "12:24",
+							"startTime": 41520,
+							"endTime": 44640
+						},
+						{
+							"name": "C Lunch",
+							"startTimeDigits": "12:24",
+							"endTimeDigits": "12:54",
+							"startTime": 44640,
+							"endTime": 46440
+						},
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "12:54",
+							"endTimeDigits": "12:59",
+							"startTime": 46440,
+							"endTime": 46740
+						},
+						{
+							"name": "Silver Period 7",
+							"startTimeDigits": "12:59",
+							"endTimeDigits": "13:24",
+							"startTime": 46740,
+							"endTime": 48240
+						}
+					],
+					"D": [
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "11:24",
+							"endTimeDigits": "11:32",
+							"startTime": 41040,
+							"endTime": 41520
+						},
+						{
+							"name": "Silver Period 7",
+							"startTimeDigits": "11:32",
+							"endTimeDigits": "12:54",
+							"startTime": 41520,
+							"endTime": 46440
+						},
+						{
+							"name": "D Lunch",
+							"startTimeDigits": "12:54",
+							"endTimeDigits": "13:24",
+							"startTime": 46440,
+							"endTime": 48240
+						}
+					]
+				},
+				"intraindex": -1
+			},
+			{
+				"name": "Passing Period",
+				"startTimeDigits": "13:24",
+				"endTimeDigits": "13:32",
+				"startTime": 48240,
+				"endTime": 48720,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Silver Period 8",
+				"startTimeDigits": "13:32",
+				"endTimeDigits": "15:00",
+				"startTime": 48720,
+				"endTime": 54000,
+				"intraschedule": {},
+				"intraindex": -1
+			}
+		]
+	},
+	"1GLL2DMNH": {
+		"metadata": {
+			"color": "rgb(102, 102, 102)",
+			"position": 1,
+			"needsCheck": false,
+			"name": "No School"
+		},
+		"periods": [
+			{
+				"name": "No School",
+				"startTimeDigits": "00:00",
+				"startTime": 0,
+				"endTimeDigits": "00:00",
+				"endTime": 0,
+				"intraschedule": {},
+				"intraindex": -1
+			}
+		]
+	},
+	"1GLL2GT41": {
+		"metadata": {
+			"name": "Red Day",
+			"color": "rgb(255, 0, 0)",
+			"needsCheck": true
+		},
+		"periods": [
+			{
+				"name": "Passing Period",
+				"startTimeDigits": "08:00",
+				"endTimeDigits": "08:30",
+				"startTime": 28800,
+				"endTime": 30600,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Red Period 1",
+				"startTimeDigits": "08:30",
+				"endTimeDigits": "09:53",
+				"startTime": 30600,
+				"endTime": 35580,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Passing Period",
+				"startTimeDigits": "09:53",
+				"endTimeDigits": "10:01",
+				"startTime": 35580,
+				"endTime": 36060,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Red Period 2",
+				"startTimeDigits": "10:01",
+				"endTimeDigits": "11:24",
+				"startTime": 36060,
+				"endTime": 41040,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Red Period 3 & Lunch",
+				"startTimeDigits": "11:24",
+				"endTimeDigits": "13:24",
+				"startTime": 41040,
+				"endTime": 48240,
+				"intraschedule": {
+					"A": [
+						{
+							"name": "A Lunch",
+							"startTimeDigits": "11:24",
+							"endTimeDigits": "11:54",
+							"startTime": 41040,
+							"endTime": 42840
+						},
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "11:54",
+							"endTimeDigits": "11:59",
+							"startTime": 42840,
+							"endTime": 43140
+						},
+						{
+							"name": "Red Period 3",
+							"startTimeDigits": "11:59",
+							"endTimeDigits": "13:24",
+							"startTime": 43140,
+							"endTime": 48240
+						}
+					],
+					"B": [
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "11:24",
+							"endTimeDigits": "11:32",
+							"startTime": 41040,
+							"endTime": 41520
+						},
+						{
+							"name": "Red Period 3",
+							"startTimeDigits": "11:32",
+							"endTimeDigits": "11:54",
+							"startTime": 41520,
+							"endTime": 42840
+						},
+						{
+							"name": "B Lunch",
+							"startTimeDigits": "11:54",
+							"endTimeDigits": "12:24",
+							"startTime": 42840,
+							"endTime": 44640
+						},
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "12:24",
+							"endTimeDigits": "12:29",
+							"startTime": 44640,
+							"endTime": 44940
+						},
+						{
+							"name": "Red Period 3",
+							"startTimeDigits": "12:29",
+							"endTimeDigits": "13:24",
+							"startTime": 44940,
+							"endTime": 48240
+						}
+					],
+					"C": [
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "11:24",
+							"endTimeDigits": "11:32",
+							"startTime": 41040,
+							"endTime": 41520
+						},
+						{
+							"name": "Red Period 3",
+							"startTimeDigits": "11:32",
+							"endTimeDigits": "12:24",
+							"startTime": 41520,
+							"endTime": 44640
+						},
+						{
+							"name": "C Lunch",
+							"startTimeDigits": "12:24",
+							"endTimeDigits": "12:54",
+							"startTime": 44640,
+							"endTime": 46440
+						},
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "12:54",
+							"endTimeDigits": "12:59",
+							"startTime": 46440,
+							"endTime": 46740
+						},
+						{
+							"name": "Red Period 3",
+							"startTimeDigits": "12:59",
+							"endTimeDigits": "13:24",
+							"startTime": 46740,
+							"endTime": 48240
+						}
+					],
+					"D": [
+						{
+							"name": "Passing Period",
+							"startTimeDigits": "11:24",
+							"endTimeDigits": "11:32",
+							"startTime": 41040,
+							"endTime": 41520
+						},
+						{
+							"name": "Red Period 3",
+							"startTimeDigits": "11:32",
+							"endTimeDigits": "12:54",
+							"startTime": 41520,
+							"endTime": 46440
+						},
+						{
+							"name": "D Lunch",
+							"startTimeDigits": "12:54",
+							"endTimeDigits": "13:24",
+							"startTime": 46440,
+							"endTime": 48240
+						}
+					]
+				},
+				"intraindex": -1
+			},
+			{
+				"name": "Passing Period",
+				"startTimeDigits": "13:24",
+				"endTimeDigits": "13:32",
+				"startTime": 48240,
+				"endTime": 48720,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Red Period 4",
+				"startTimeDigits": "13:32",
+				"endTimeDigits": "15:00",
+				"startTime": 48720,
+				"endTime": 54000,
+				"intraschedule": {},
+				"intraindex": -1
+			}
+		]
+	},
+	"1GLL2JFJU": {
+		"metadata": {
+			"color": "rgb(229, 255, 0)",
+			"needsCheck": false,
+			"position": 4,
+			"name": "eLearning"
+		},
+		"periods": [
+			{
+				"name": "eLearning",
+				"startTimeDigits": "00:00",
+				"startTime": 0,
+				"endTimeDigits": "00:00",
+				"endTime": 0,
+				"intraschedule": {},
+				"intraindex": -1
+			}
+		]
+	},
+	"1GLL2LH9H": {
+		"metadata": {
+			"color": "rgb(0, 255, 51)",
+			"position": 5,
+			"needsCheck": true,
+			"name": "Finals Day"
+		},
+		"periods": [
+			{
+				"name": "Passing Period",
+				"startTimeDigits": "08:00",
+				"startTime": 28800,
+				"endTimeDigits": "08:30",
+				"endTime": 30600,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Teacher Office Hourss",
+				"startTimeDigits": "08:30",
+				"startTime": 30600,
+				"endTimeDigits": "10:40",
+				"endTime": 38400,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Lunch",
+				"startTimeDigits": "10:40",
+				"startTime": 38400,
+				"endTimeDigits": "11:10",
+				"endTime": 40200,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Passing Period",
+				"startTimeDigits": "11:10",
+				"startTime": 40200,
+				"endTimeDigits": "11:20",
+				"endTime": 40800,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Final",
+				"startTimeDigits": "11:20",
+				"startTime": 40800,
+				"endTimeDigits": "13:05",
+				"endTime": 47100,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Passing Period",
+				"startTimeDigits": "13:05",
+				"startTime": 47100,
+				"endTimeDigits": "13:15",
+				"endTime": 47700,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Final",
+				"startTimeDigits": "13:15",
+				"startTime": 47700,
+				"endTimeDigits": "15:00",
+				"endTime": 54000,
+				"intraschedule": {},
+				"intraindex": -1
+			}
+		]
+	},
+	"1GO97QA7J": {
+		"metadata": {
+			"name": "Seven Period Day",
+			"color": "rgb(255, 140, 0)",
+			"needsCheck": true
+		},
+		"periods": [
+			{
+				"name": "Period 1 ",
+				"startTimeDigits": "08:30",
+				"endTimeDigits": "09:13",
+				"startTime": 30600,
+				"endTime": 33180,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Period 2 ",
+				"startTimeDigits": "09:21",
+				"endTimeDigits": "10:04",
+				"startTime": 33660,
+				"endTime": 36240,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Period 3",
+				"startTimeDigits": "10:12",
+				"endTimeDigits": "10:55",
+				"startTime": 36720,
+				"endTime": 39300,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Period 4 ",
+				"startTimeDigits": "11:03",
+				"endTimeDigits": "11:46",
+				"startTime": 39780,
+				"endTime": 42360,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Period 5 (Lunch)",
+				"startTimeDigits": "11:46",
+				"endTimeDigits": "13:16",
+				"startTime": 42360,
+				"endTime": 47760,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Period 6 ",
+				"startTimeDigits": "13:24",
+				"endTimeDigits": "14:08",
+				"startTime": 48240,
+				"endTime": 50880,
+				"intraschedule": {},
+				"intraindex": -1
+			},
+			{
+				"name": "Period 7",
+				"startTimeDigits": "14:16",
+				"endTimeDigits": "15:00",
+				"startTime": 51360,
+				"endTime": 54000,
+				"intraschedule": {},
+				"intraindex": -1
+			}
+		]
+	},
+	"1GO98I27F": {
+		"metadata": {
+			"name": "SAT Day",
+			"color": "rgb(0, 255, 191)",
+			"needsCheck": true
+		},
+		"periods": [
+			{
+				"name": "SAT Day",
+				"startTimeDigits": "08:30",
+				"endTimeDigits": "15:00",
+				"startTime": 30600,
+				"endTime": 54000,
+				"intraschedule": {},
+				"intraindex": -1
+			}
+		]
+	}
+}
