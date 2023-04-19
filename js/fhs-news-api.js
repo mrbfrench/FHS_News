7e3ceea64f3894d577710767af0799dd20a9e801
Splittikin <45697042+Splittikin@users.noreply.github.com>
2023-04-19 09:44:35 -0400

    Pull modules in from SchedLink

---
+++ js/fhs-news-api.js
@@ -0,0 +1,19 @@
+console.log("testy westy");
+
+/*
+async function get() {
+	let obj = fetch('http://localhost:3000/api/home')
+
+	return obj
+}
+
+get().then(obj =>
+	console.log(obj.text().valueOf())
+)
+*/
+
+//let mainContent = document.getElementById("main-content")
+//console.log(mainContent)
+
+//let element = document.createElement("section")
+//mainContent.appendChild(element)
