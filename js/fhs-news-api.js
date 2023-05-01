5f2df57bb998dbc741aa2394d0127242efcfeb50
Splittikin <45697042+Splittikin@users.noreply.github.com>
2023-05-01 18:27:05 -0400

    Testing appending modules via JS

--- js/fhs-news-api.js
+++ js/fhs-news-api.js
@@ -17,3 +17,35 @@ get().then(obj =>
 
 //let element = document.createElement("section")
 //mainContent.appendChild(element)
+
+/**
+ * Returns an array with each entry representing a card in the home feed
+ * @returns {Promise<Array<Object>>}
+ */
+async function getHome(position = 0, quantity = 5) {
+	// noinspection JSCheckFunctionSignatures
+	const targetUrl = 'http://localhost:3000/api/home?' + new URLSearchParams({
+		position: position, quantity: quantity
+	})
+
+	return fetch(targetUrl).then(response => response.json()).then(response => {
+		return response
+	})
+}
+
+// TODO: Load each module in the folder once and pull from there instead of dinging the module location for each and every card
+
+window.addEventListener('load', function () {
+	// Runs when the webpage finishes loading
+
+	// This Is A Test
+	let mainContent = document.getElementById("main-content")
+	let testAlert = document.createElement("section")
+	testAlert.className = "block"
+	fetch('http://localhost:63342/FHSnewsWeb/modules/alert.html').then(response => response.text()).then(response => testAlert.innerHTML = response)
+	mainContent.appendChild(testAlert)
+	// This Is A Test
+
+})
+
+
